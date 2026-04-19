8c9a050aad7e3768229d29694f931e06cbeb8e99
insynia-agent <insynia-agent@insynia.com>
2026-04-19 19:25:16 +0000

    Remove team section from front page template

--- themes/insynia/front-page.php
+++ themes/insynia/front-page.php
@@ -11,7 +11,6 @@ get_header();
 <?php get_template_part( 'template-parts/hero' ); ?>
 <?php get_template_part( 'template-parts/products' ); ?>
 <?php get_template_part( 'template-parts/about' ); ?>
-<?php get_template_part( 'template-parts/team' ); ?>
 <?php get_template_part( 'template-parts/cta' ); ?>
 
 <?php get_footer(); ?>
