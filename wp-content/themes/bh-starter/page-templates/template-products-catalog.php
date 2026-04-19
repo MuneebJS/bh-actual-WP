6c031f90502c3275fb5887f192742439f87c661f
Muneeb Khan <munibkhan7737@gmail.com>
2026-04-20 00:23:44 +0500

    Update

--- wp-content/themes/bh-starter/page-templates/template-products-catalog.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Template Name: Products catalog
- * Description: Lists all catalog products with links to detail pages.
- *
- * @package BH_Starter
- */
-
-require get_template_directory() . '/products-catalog.php';
